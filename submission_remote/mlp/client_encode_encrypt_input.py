4ff5e82bfbe7705e88aabd5f6029ffb3e5760460
pavel <pavel@lattica.ai>
2026-05-08 16:02:24 +0300

    Update code to support lattice-query client 1.5.0

--- submission_remote/mlp/client_encode_encrypt_input.py
+++ submission_remote/mlp/client_encode_encrypt_input.py
@@ -2,9 +2,7 @@ import pickle
 import sys
 
 import lattica_query.query_toolkit as toolkit_interface
-from lattica_query.serialization.hom_op_pb2 import (
-    QueryClientSequentialHomOp as ProtoQueryClientSequentialHomOp,
-)
+from lattica_query.serialization.hom_op_pb2 import ClientModel as ProtoClientModel
 
 import submission_utils
 local_file_paths = submission_utils.init(int(sys.argv[1]))
@@ -13,15 +11,16 @@ local_file_paths = submission_utils.init(int(sys.argv[1]))
 with open(local_file_paths.PT_PATH, "rb") as f:
     pt_ser = f.read()
 
-context = pickle.load(open(local_file_paths.PATH_CONTEXT, "rb"))
-hom_seq = pickle.load(open(local_file_paths.PATH_HOM_SEQ, "rb"))
+context_ser = pickle.load(open(local_file_paths.PATH_CONTEXT, "rb"))
+model_ser = pickle.load(open(local_file_paths.PATH_HOM_SEQ, "rb"))
 sk =      pickle.load(open(local_file_paths.PATH_SK, "rb"))
 
-homsec_proto = ProtoQueryClientSequentialHomOp()
-homsec_proto.ParseFromString(hom_seq)
-block_proto = homsec_proto.client_blocks[0]
-pt_axis_external = block_proto.pt_axis_external if block_proto.HasField("pt_axis_external") else None
+model_proto = ProtoClientModel()
+model_proto.ParseFromString(model_ser)
 
-pt_enc = toolkit_interface.apply_client_block(block_proto.SerializeToString(), context, pt_ser)
-ct_batch = toolkit_interface.enc(context, sk, pt_enc, pack_for_transmission=True, n_axis_external=pt_axis_external)
+preprocess_block_ser = model_proto.preprocess_block.SerializeToString()
+pt_axis_external = model_proto.pt_axis_external if model_proto.HasField("pt_axis_external") else None
+
+pt_enc = toolkit_interface.apply_client_block(preprocess_block_ser, context_ser, pt_ser)
+ct_batch = toolkit_interface.enc(context_ser, sk, pt_enc, pack_for_transmission=True, n_axis_external=pt_axis_external)
 pickle.dump(ct_batch, open(local_file_paths.get_ct_upload_path("batch"), "wb"))
